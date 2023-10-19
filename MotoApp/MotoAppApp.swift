75841b0112a8026128b358f77c50b6534c666cde
Naren Bollineni <75765293+bollineni-create@users.noreply.github.com>
2023-10-19 09:46:44 -0400

    Add files via upload

---
+++ MotoApp/MotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotoAppApp.swift
+//  MotoApp
+//
+//  Created by Naren Sai Bollineni on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
